4f28cce83e37a33f5072079077fdf1f161948c13
CarlVenter-Git <carlventer@live.co.za>
2019-08-27 21:35:41 +0200

    Working platform application, and first Git commit

---
+++ CircleMath/Main.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CircleMath
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTimesTable = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCircleDivisions = new System.Windows.Forms.TextBox();
+            this.doMath = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(594, 473);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtTimesTable);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtCircleDivisions);
+            this.panel2.Controls.Add(this.doMath);
+            this.panel2.Location = new System.Drawing.Point(0, 472);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(594, 89);
+            this.panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Times table";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of divisions";
+            // 
+            // txtTimesTable
+            // 
+            this.txtTimesTable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTimesTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimesTable.Location = new System.Drawing.Point(260, 7);
+            this.txtTimesTable.Name = "txtTimesTable";
+            this.txtTimesTable.Size = new System.Drawing.Size(94, 53);
+            this.txtTimesTable.TabIndex = 3;
+            this.txtTimesTable.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X";
+            // 
+            // txtCircleDivisions
+            // 
+            this.txtCircleDivisions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtCircleDivisions.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCircleDivisions.Location = new System.Drawing.Point(12, 7);
+            this.txtCircleDivisions.Name = "txtCircleDivisions";
+            this.txtCircleDivisions.Size = new System.Drawing.Size(94, 53);
+            this.txtCircleDivisions.TabIndex = 1;
+            this.txtCircleDivisions.Text = "10";
+            // 
+            // doMath
+            // 
+            this.doMath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.doMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doMath.Location = new System.Drawing.Point(488, 7);
+            this.doMath.Name = "doMath";
+            this.doMath.Size = new System.Drawing.Size(94, 70);
+            this.doMath.TabIndex = 0;
+            this.doMath.Text = "Go";
+            this.doMath.UseVisualStyleBackColor = true;
+            this.doMath.Click += new System.EventHandler(this.DoMath_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 561);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.panel2);
+            this.MinimumSize = new System.Drawing.Size(610, 600);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button doMath;
+        private System.Windows.Forms.TextBox txtTimesTable;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCircleDivisions;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CircleMath/Main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleMath
+{
+    public partial class Main : Form
+    {
+        static Thread thread;
+        static Bitmap bitmap;
+
+        static Int16 timesTable = 0; //for testing
+        static Double theta = 0;
+        static Double increment = 0; //number of points on circle circumfrence
+        static float radius = 0.0f;
+
+        static PointF originPoint = new PointF(0, 0);
+        static PointF pointStart = new PointF(0, 0);//Where the bitmap being displayed will start
+        static PointF nextPoint = Point.Empty;
+
+        RectangleF circlePoint = new RectangleF(0, 0, 0, 0);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Main_Resize(sender, e);//Get the intial size of the canvas
+        }
+
+        private void DoMath_Click(object sender, EventArgs e)
+        {
+            Main_Resize(sender, e);//making sure the canvas is the correct size when the go button is clicked
+
+            timesTable = Int16.Parse(txtTimesTable.Text);
+            theta = 360 / Int64.Parse(txtCircleDivisions.Text);//workes out the number of evenly sized slices needed
+            increment = theta;//increment to store the angle of each section of the circle
+            
+            bitmap = new Bitmap(canvas.Width, canvas.Height);
+
+            thread = new Thread(Draw);//Creating a new thread to avoid locking UI and prevent the image from flickering
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void Draw()
+        {
+            Bitmap buffer = new Bitmap(canvas.Width, canvas.Height);
+            var valuePairs = new Dictionary<Int64, PointF>();
+            Int64 temp = 0;
+
+            canvas.BackColor = Color.Black;
+
+            do
+            {
+                //points.Add(CirclePoint(radius, theta, originPoint));
+                valuePairs.Add(temp, CirclePoint(radius, theta, originPoint));
+
+                temp++;
+                theta += increment;
+            } while (theta <= 360);
+
+            //Draw a line between two points
+            using (Graphics g = Graphics.FromImage(buffer))
+            using (Pen pen = new Pen(Color.White))
+            {
+                for (int i = 0; i < valuePairs.Count; i++)
+                {
+                    temp = (i * timesTable) % 10;//gets last digit of result as index of line destination
+                    nextPoint = valuePairs[temp];
+
+                    g.DrawLine(pen, valuePairs[i], nextPoint);
+
+                    lock (bitmap)
+                    {
+                        bitmap = buffer;
+                    }
+                }
+            }
+
+            canvas.Invalidate();//So the last line gets drawn
+        }
+
+        private PointF CirclePoint(float radius, double theta, PointF originPoint)
+        {
+            float x = -(float)(radius * Math.Cos(theta * Math.PI / 180f)) + originPoint.X;
+            float y = -(float)(radius * Math.Sin(theta * Math.PI / 180f)) + originPoint.Y;
+
+            return new PointF(x, y);
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (bitmap != null)
+            {
+                e.Graphics.DrawImage(bitmap, pointStart);
+            }
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            canvas.Height = this.Height - (panel2.Height * 2);
+            canvas.Width = this.Width;
+            originPoint.X = canvas.Width / 2;
+            originPoint.Y = canvas.Height / 2;
+
+            radius = (canvas.Height / 2) - 50; //keep the point a bit from the edge of the canvas, we have less height than width
+        }
+    }
+}
